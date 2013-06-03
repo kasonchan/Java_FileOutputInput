f36ecbeaa6cac50db147c8110fec32f955497e3e
Kason Chan <kasonl.chan@gmail.com>
2013-06-02 23:12:44 -0500

    Update files

--- FileOI.java
+++ FileOI.java
@@ -7,7 +7,7 @@ public class FileOI {
   public static void main(String[] args) {
     int i = 0;
     Boolean loop = true;
-    int numOfFiles = args.length;
+    int numOfFiles = args.length - 2;
     String[] fileNames;
     String[] lines;
     Scanner keyboard  = new Scanner(System.in);
@@ -17,65 +17,67 @@ public class FileOI {
     fileNames = new String[numOfFiles];
     lines = new String[numOfFiles];
 
-    for (i = 0; i < numOfFiles; i++) {
-      fileNames[i] = args[i];
-    }
-
-    // Declare input stream
-    Scanner[] inputStreams = new Scanner[numOfFiles];
-    
-    for (i = 0; i < numOfFiles; i++) {
-      try {
-        // Open file
-        inputStreams[i] = new Scanner(new File(fileNames[i]));
+    if (args[0].toLowerCase().equals("join")) {   
+      for (i = 0; i < numOfFiles; i++) {
+        fileNames[i] = args[i + 2];
       }
-      catch (FileNotFoundException e) {
-        // If can not open file
-        System.out.println("Error opening the file " + fileNames[i]);
 
+      // Declare input stream
+      Scanner[] inputStreams = new Scanner[numOfFiles];
+      
+      for (i = 0; i < numOfFiles; i++) {
+        try {
+          // Open file
+          inputStreams[i] = new Scanner(new File(fileNames[i]));
+        } 
+        catch (FileNotFoundException e) {
+          // If can not open file
+          System.out.println("Error opening the file " + fileNames[i]);
+
+          System.exit(-1);
+        }
+      }
+
+      PrintWriter outputStream = null;
+
+      try {
+        outputStream = new PrintWriter(outputFileName);
+      }
+      catch(FileNotFoundException e) {
+        System.out.println("Error opening the file " + outputFileName);
         System.exit(-1);
       }
-    }
 
-    PrintWriter outputStream = null;
+      do {
+        for (i = 0; i < numOfFiles; i++) {
+          if (inputStreams[i].hasNextLine()) {
+            // Read line from file and print it
+            lines[i] = inputStreams[i].nextLine();
+            System.out.print(lines[i]);
+            outputStream.print(lines[i]);
+            if (i < numOfFiles - 1) {
+              System.out.print(args[1] + " ");
+              outputStream.print(args[1] + " ");
+            }
+            else { 
+              System.out.println(" ");
+              outputStream.println(" ");
+            }
 
-    try {
-      outputStream = new PrintWriter(outputFileName);
-    }
-    catch(FileNotFoundException e) {
-      System.out.println("Error opening the file " + outputFileName);
-      System.exit(-1);
-    }
+          }
+          else {
+            loop = false;
+            break;
+          }
+        }
+      } while (loop);
 
-    do {
       for (i = 0; i < numOfFiles; i++) {
-        if (inputStreams[i].hasNextLine()) {
-          // Read line from file and print it
-          lines[i] = inputStreams[i].nextLine();
-          System.out.print(lines[i]);
-          outputStream.print(lines[i]);
-          if (i < numOfFiles - 1) {
-            System.out.print(", ");
-            outputStream.print(", ");
-          }
-          else { 
-            System.out.println(" ");
-            outputStream.println(" ");
-          }
-
-        }
-        else {
-          loop = false;
-          break;
-        }
+        inputStreams[i].close();
       }
-    } while (loop);
 
-    for (i = 0; i < numOfFiles; i++) {
-      inputStreams[i].close();
+      outputStream.close();
     }
-
-    outputStream.close();
 
     System.exit(0);
   }
